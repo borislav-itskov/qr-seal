ef128c3465fed34747e05d3be85121a964e7038c
Kaloyan Kosev <superkalo@devlabs.bg>
2023-06-09 14:45:18 +0200

    Add: wrap up lib in modal

--- src/multisig/components/CreateMultisigByScanning.tsx
+++ src/multisig/components/CreateMultisigByScanning.tsx
@@ -1,14 +1,30 @@
+import {
+  Button,
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  useDisclosure,
+} from "@chakra-ui/react";
 import QRCodeScanner from "../../common/QRCodeScanner";
 
-const CreateMultisigByScanning = (props) => {
-  const handleScanSuccess = (scan) => console.log(scan);
-  const handleScanError = (error) => console.log(error);
+const CreateMultisigByScanning = (props: any) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const handleScanSuccess = (scan: any) => console.log(scan);
+  const handleScanError = (error: any) => console.log(error);
 
   return (
-    <QRCodeScanner
-      qrCodeSuccessCallback={handleScanSuccess}
-      qrCodeErrorCallback={handleScanError}
-    />
+    <>
+      <Button onClick={onOpen}>Create Multisig</Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <QRCodeScanner
+            qrCodeSuccessCallback={handleScanSuccess}
+            qrCodeErrorCallback={handleScanError}
+          />
+        </ModalContent>
+      </Modal>
+    </>
   );
 };
 
