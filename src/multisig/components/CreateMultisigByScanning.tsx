0fec6e552ee5815dd74451de8127dcd2e64fce48
Kaloyan Kosev <superkalo@devlabs.bg>
2023-06-09 16:46:30 +0200

    Add: multisig creation logic

--- src/multisig/components/CreateMultisigByScanning.tsx
+++ src/multisig/components/CreateMultisigByScanning.tsx
@@ -7,9 +7,22 @@ import {
 } from "@chakra-ui/react";
 import QRCodeScanner from "../../common/QRCodeScanner";
 import { useState } from "react";
+import Schnorrkel, { Key } from "@borislav.itskov/schnorrkel.js";
+import { getAmbireAccountAddress } from "../../utils/helpers";
+import buildinfo from "../../builds/FactoryAndAccountBuild.json";
+import {
+  getProxyDeployBytecode,
+  getStorageSlotsFromArtifact,
+} from "../../deploy/getBytecode";
+import { ethers } from "ethers";
+import { getEOAPublicKey } from "../../auth/services/eoa";
+
+// TODO: Change those when we deploy on a specific network
+const FACTORY_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const AMBIRE_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 
 const CreateMultisigByScanning = (props: any) => {
-  const [multisigPublicAddress, setMultisigPublicAddress] = useState(false);
+  const [multisigPublicAddress, setMultisigPublicAddress] = useState("");
   const { isOpen, onOpen, onClose } = useDisclosure();
   const handleScanSuccess = (scan: any = "") => {
     const data = scan.split("|");
@@ -21,18 +34,42 @@ const CreateMultisigByScanning = (props: any) => {
       return;
     }
 
+    const publicKey = getEOAPublicKey();
     const multisigPartnerPublicKey = data[0];
     const multisigPartnerKPublicHex = data[1];
     const multisigPartnerKTwoPublicHex = data[2];
 
-    // TODO: Create multisig!
-    console.log(
-      "Scanned!",
-      multisigPartnerPublicKey,
-      multisigPartnerKPublicHex,
-      multisigPartnerKTwoPublicHex
-    );
     onClose();
+
+    const publicKeyOne = new Key(Buffer.from(ethers.utils.arrayify(publicKey)));
+    const publicKeyTwo = new Key(
+      Buffer.from(ethers.utils.arrayify(multisigPartnerPublicKey))
+    );
+
+    try {
+      const combinedPublicKey = Schnorrkel.getCombinedPublicKey([
+        publicKeyOne,
+        publicKeyTwo,
+      ]);
+      const px = ethers.utils.hexlify(combinedPublicKey.buffer.slice(1, 33));
+      const schnorrHash = ethers.utils.keccak256(
+        ethers.utils.solidityPack(["string", "bytes"], ["SCHNORR", px])
+      );
+      const schnorrVirtualAddr =
+        "0x" + schnorrHash.slice(schnorrHash.length - 40, schnorrHash.length);
+
+      const bytecode = getProxyDeployBytecode(
+        AMBIRE_ADDRESS,
+        [{ addr: schnorrVirtualAddr, hash: true }],
+        {
+          ...getStorageSlotsFromArtifact(buildinfo),
+        }
+      );
+      const multisigAddr = getAmbireAccountAddress(FACTORY_ADDRESS, bytecode);
+      setMultisigPublicAddress(multisigAddr);
+    } catch (e) {
+      console.log("The multisig creation failed", e);
+    }
   };
   const handleScanError = (error: any) => console.error(error);
 
