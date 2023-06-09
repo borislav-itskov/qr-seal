7918a598d287ff3fb54c411cc79ccb207db59b14
Kaloyan Kosev <superkalo@devlabs.bg>
2023-06-09 12:19:41 +0200

    Move the init logic before <App

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,7 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Sign from "./sign/Sign";
-import { createAndStoreEOAIfNeeded } from "./auth/services/eoa";
 import EOAAccount from "./auth/components/EOAAccount";
-
-// TODO: Init EOA account creation when page is loaded. Ideally, it should
-// prompt the user to create EOA or something.
-createAndStoreEOAIfNeeded();
 
 function App() {
   return (
