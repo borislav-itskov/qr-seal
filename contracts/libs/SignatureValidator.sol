c91c08f64a7f78fe0995e21c8ddacdc472c408c2
Borislav Itskov <borislav@devlabs.bg>
2023-06-10 20:57:55 +0300

    remove console from AmbireAccount

--- contracts/libs/SignatureValidator.sol
+++ contracts/libs/SignatureValidator.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.19;
 
 import './Bytes.sol';
-import 'hardhat/console.sol';
 
 interface IERC1271Wallet {
 	function isValidSignature(bytes32 hash, bytes calldata signature) external view returns (bytes4 magicValue);
